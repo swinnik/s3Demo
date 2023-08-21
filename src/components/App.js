eb9f1da543f4fe85a8cb122d2d929f04cc7bb021
Sean Winnik <winnik.sean@gmail.com>
2023-08-21 13:30:05 -0400

    attempting to deploy /dist only

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Title> HELLO WORLD!!! </Title>
+        <Title> CI/CD?? IT WORKS!!! </Title>
       </div>
     );
   }
